67101fe8dc33e74aa49d4d0f41e4472a23bf90d4
Eduard Krasnov <eduard.krasnov1@hm.edu>
2024-01-11 20:36:02 +0100

    .

--- src/components/InspectorAnswer.jsx
+++ src/components/InspectorAnswer.jsx
@@ -6,7 +6,7 @@ const InspectorAnswer = ({ content, handleClickEvent, handleCloseButton }) => {
   const { t, i18n } = useTranslation();
   let contentTime = content.CTime;
   let contentDate = content.CDate;
-
+  console.log(content);
   return (
     <div
       className="
